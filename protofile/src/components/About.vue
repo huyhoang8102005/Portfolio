306017c02a320c6c6d225df86d10294bac087c5b
huyhoang <tonghuuhuyhoang2005@gmail.com>
2025-02-20 07:15:55 +0700

    completed About.vue

--- protofile/src/components/About.vue
+++ protofile/src/components/About.vue
@@ -1,138 +1,140 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-	<section id="about">
-		<div class="container">
-			<div class="about__container">
-				<div class="about__top">
-					<h2 class="about__top-title">About Me</h2>
-					<p class="about__top-desc">My introduction</p>
-				</div>
-				<div class="about__content">
-					<div class="about__left">
-						<img src="../assets/img/avatar.jpg" alt="" class="about__left-avatar">
-					</div>
-					<div class="about__right">
-						<div class="about__right-list">
-							<div class="about__right-item">
-								<img src="../assets/icons/idea.svg" alt="" class="icon">
-								<span class="about__right-item--title">Experience</span>
-								<span class="about__right-item--desc">2+ Years</span>
-							</div>
-							<div class="about__right-item">
-								<img src="../assets/icons/bag.svg" alt="" class="icon">
-								<span class="about__right-item--title">Completed</span>
-								<span class="about__right-item--desc">2 Project</span>
-							</div>
-							<div class="about__right-item">
-								<img src="../assets/icons/support.svg" alt="" class="icon">
-								<span class="about__right-item--title">Support</span>
-								<span class="about__right-item--desc">Online 24/7</span>
-							</div>
-						</div>
-						<div class="about__right-desc">
-							<p>FullStack Developer, I create web pages with UI/UX user interface as well as create the backend server.
-								I have two years experience for doing with framework Vue, js, html and css</p>
-						</div>
-						<div class="about__btn button has-icon">
-							<span>Download CV</span>
-							<img src="../assets/icons/send.svg" alt="" class="icon icon-light" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+  <section id="about">
+    <div class="container">
+      <div class="about__container">
+        <div class="about__top">
+          <h2 class="about__top-title">About Me</h2>
+          <p class="about__top-desc">My introduction</p>
+        </div>
+        <div class="about__content">
+          <div class="about__left">
+            <img src="../assets/img/avatar.jpg" alt="" class="about__left-avatar" />
+          </div>
+          <div class="about__right">
+            <div class="about__right-list">
+              <div class="about__right-item">
+                <img src="../assets/icons/idea.svg" alt="" class="icon" />
+                <span class="about__right-item--title">Experience</span>
+                <span class="about__right-item--desc">2+ Years</span>
+              </div>
+              <div class="about__right-item">
+                <img src="../assets/icons/bag.svg" alt="" class="icon" />
+                <span class="about__right-item--title">Completed</span>
+                <span class="about__right-item--desc">2 Project</span>
+              </div>
+              <div class="about__right-item">
+                <img src="../assets/icons/support.svg" alt="" class="icon" />
+                <span class="about__right-item--title">Support</span>
+                <span class="about__right-item--desc">Online 24/7</span>
+              </div>
+            </div>
+            <div class="about__right-desc">
+              <p>
+                FullStack Developer, I create web pages with UI/UX user interface as well as create
+                the backend server. I have two years experience for doing with framework Vue, js,
+                html and css
+              </p>
+            </div>
+            <div class="about__btn button has-icon">
+              <span>Download CV</span>
+              <img src="../assets/icons/send.svg" alt="" class="icon icon-light" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
 #about {
-	margin-top: 350px;
+  margin-top: 350px;
 }
 
 .about__top {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	gap: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 14px;
 }
 
 .about__top-title {
-	font-size: 52px;
-	font-weight: 500;
+  font-size: 52px;
+  font-weight: 500;
 }
 
 .about__top-desc {
-	color: var(--text-color);
+  color: var(--text-color);
 }
 
 .about__content {
-	margin-top: 80px;
-	display: flex;
-	align-items: center;
-	gap: 100px;
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  gap: 100px;
 }
 
 .about__left {
-	flex: 0.5;
-	border-radius: 20px;
-	overflow: hidden;
+  flex: 0.5;
+  border-radius: 20px;
+  overflow: hidden;
 }
 
 .about__left-avatar {
-	width: 100%;
-	height: auto;
-	object-fit: contain;
+  width: 100%;
+  height: auto;
+  object-fit: contain;
 }
 
 .about__right {
-	flex: 1;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	gap: 20px;
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
 .about__right-list {
-	display: flex;
-	align-items: center;
-	gap: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
 }
 
 .about__right-item {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
 
-	width: 240px;
-	background: rgba(204, 204, 204, 0.3);
-	border-radius: 10px;
+  width: 240px;
+  background: rgba(204, 204, 204, 0.3);
+  border-radius: 10px;
 
-	padding: 40px;
+  padding: 40px;
 }
 
 .about__right-item--title {
-	font-size: 20px;
+  font-size: 20px;
 }
 
 .about__right-desc {
-	margin-top: 20px;
-	line-height: 32px;
-	color: var(--text-color);
-	font-size: 24px;
+  max-width: 98%;
+  margin-top: 20px;
+  line-height: 32px;
+  color: var(--text-color);
+  font-size: 22px;
 }
 
 .about__btn {
-	width: 220px;
-	margin-top: 30px;
+  width: 220px;
+  margin-top: 30px;
 }
 
 .has-icon {
-	gap: 20px;
+  gap: 20px;
 }
 </style>
--- protofile/src/components/Skill.vue
+++ protofile/src/components/Skill.vue
@@ -1,41 +1,199 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <section id="skill">
-        <div class="container">
-            <div class="skill__container">
-                <div class="skill__top">
-                    <h2 class="skill__top-title">Skiils</h2>
-                    <p class="skill__top-desc">My technical level</p>
-                </div>
-            </div>
-            <div class="skiil__content"></div>
+  <section id="skill">
+    <div class="container">
+      <div class="skill__container">
+        <div class="skill__top">
+          <h2 class="skill__top-title">Skiils</h2>
+          <p class="skill__top-desc">My technical level</p>
         </div>
-    </section>
+        <div class="skiil__content">
+          <div class="skill__content-block">
+            <h3 class="skill__content-block--h3">Frontend developer</h3>
+            <div class="skill__content-block--container">
+              <div class="skill__content-col">
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">HTML</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">CSS</span>
+                    <span class="skill__content-item--desc">Advanced</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">JavaScript</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+              </div>
+              <div class="skill__content-col">
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">Bootstrap</span>
+                    <span class="skill__content-item--desc">Basic</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">Git</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">Vue</span>
+                    <span class="skill__content-item--desc">Advanced</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="skill__content-block">
+            <h3 class="skill__content-block--h3">Backend developer</h3>
+            <div class="skill__content-block--container">
+              <div class="skill__content-col">
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">Node Js</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">MySQL</span>
+                    <span class="skill__content-item--desc">Basic</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">MongoDB</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+              </div>
+              <div class="skill__content-col">
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">FireBase</span>
+                    <span class="skill__content-item--desc">Basic</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">TypeScript</span>
+                    <span class="skill__content-item--desc">Intermediate</span>
+                  </div>
+                </div>
+                <div class="skill__content-item">
+                  <img src="../assets/icons/circle-check.svg" alt="" class="icon-medium" />
+                  <div class="skill__content-item--container">
+                    <span class="skill__content-item--title">SQL</span>
+                    <span class="skill__content-item--desc">Basic</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
 #skill {
-    margin-top: 150px;
+  margin-top: 200px;
 }
 
 .skill__top {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 14px;
+}
+
+.skiil__content {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 100px;
 }
 
 .skill__top-title {
-    font-size: 52px;
-    font-weight: 500;
+  font-size: 52px;
+  font-weight: 500;
 }
 
 .skill__top-desc {
-    color: var(--text-color);
+  color: var(--text-color);
+}
+
+.skill__content-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 550px;
+  padding: 20px;
+  height: auto;
+  border-radius: 16px;
+  background: rgba(204, 204, 204, 0.3);
+}
+
+.skill__content-block--h3 {
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 40px;
+}
+
+.skill__content-block--container {
+  display: flex;
+  gap: 150px;
+}
+
+.skill__content-col {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.skill__content-item {
+  display: flex;
+  gap: 10px;
+}
+
+.skill__content-item--container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.skill__content-item--title {
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.skill__content-item--desc {
+  font-size: 13px;
+  color: var(--text-color);
 }
 </style>
